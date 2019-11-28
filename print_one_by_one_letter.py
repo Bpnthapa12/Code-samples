5c1118490cb54954ef0a58fa27d2136f466cdb19
Bpnthapa12 <54973420+Bpnthapa12@users.noreply.github.com>
2019-11-27 20:23:46 -0800

    2nd change for printing one by one

--- print_one_by_one_letter.py
+++ print_one_by_one_letter.py
@@ -1,6 +1,11 @@
-array= ["bipin", "subodh", "calyan"]
-for i in array:
-   length = len(i)
-   print("")
-   for j in range(0,length):
-       print(i[j])
+temp = []
+ask = int(input("Enter how many name do you want to enter? "))
+for i in range(1,ask+1):
+    ask_2 = input(f"Enter {i} name:- ")
+    temp.append(ask_2)
+print(temp)
+for j in temp:
+    length = len(j)
+    print("")
+    for k in range(0,length):
+        print(j[k])
